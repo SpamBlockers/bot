2e573a5c02f9dfe8966766b2bff4ca7b632a15b8
YouTwitFace <contact@lungers.com>
2019-10-22 23:20:47 -0400

    Allow updating gban reasons

--- handlers/gban.js
+++ handlers/gban.js
@@ -34,6 +34,12 @@ module.exports = (bot, db) => {
             );
         }
 
+        const insertedUser = {
+            user_id: id,
+            banned_by: ctx.from.id,
+            reason,
+        };
+
         db.users.findOne({ user_id: id }, (err, user) => {
             if (err) {
                 console.log(err);
@@ -44,19 +50,52 @@ module.exports = (bot, db) => {
                     })
                 );
             } else if (user) {
-                return ctx.reply(
-                    createLogMessage({
-                        header: `Error`,
-                        message: `That user is already banned`,
-                    })
+                return db.users.update(
+                    { user_id: id },
+                    { $set: { reason } },
+                    {},
+                    async err => {
+                        if (err) {
+                            console.log(err);
+                            return ctx.repy(
+                                createLogMessage({
+                                    header: `Error`,
+                                    message: err.message,
+                                })
+                            );
+                        }
+
+                        let tgUser;
+                        try {
+                            tgUser = await bot.telegram.getChat(id);
+                        } catch (_) {
+                            tgUser = null;
+                        }
+
+                        const message = createLogMessage({
+                            header: `Reason Update`,
+                            admin: mention(ctx.from, true),
+                            user: tgUser
+                                ? mention(tgUser, true)
+                                : `Unknown User (<code>${id}</code>)`,
+                            oldReason: user.reason
+                                ? escapeHtml(user.reason)
+                                : `<i>No reason specified</i>`,
+                            newReason: reason
+                                ? escapeHtml(reason)
+                                : `<i>No reason specified</i>`,
+                        });
+
+                        ctx.reply(message);
+
+                        if (LOG_CHANNEL != ctx.chat.id) {
+                            bot.telegram.sendMessage(LOG_CHANNEL, message, {
+                                parse_mode: `html`,
+                            });
+                        }
+                    }
                 );
             }
-
-            const insertedUser = {
-                user_id: id,
-                banned_by: ctx.from.id,
-                reason,
-            };
 
             db.users.insert(insertedUser, err => {
                 if (err) {
