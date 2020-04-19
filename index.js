a488d1ae348dcf388302ab9b87e89730be4efac6
YouTwitFace <contact@lungers.com>
2020-04-19 16:38:17 -0400

    Wait for MongoDB to connect before starting the bot

--- index.js
+++ index.js
@@ -4,23 +4,27 @@ const mongoose = require(`mongoose`);
 const Telegraf = require(`telegraf`);
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-});
-
-bot.context.reply = function(text, extra = {}) {
-    this.assert(this.chat, `reply`);
-
-    return this.telegram.sendMessage(this.chat.id, text, {
-        parse_mode: `html`,
-        ...extra,
+(async () => {
+    await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
     });
-};
 
-require(`./handlers`)(bot);
+    console.log(`Connected to MongoDB`);
 
-bot.launch().then(() => {
-    console.log(`@${bot.context.botInfo.username} is running...`);
-});
+    bot.context.reply = function (text, extra = {}) {
+        this.assert(this.chat, `reply`);
+
+        return this.telegram.sendMessage(this.chat.id, text, {
+            parse_mode: `html`,
+            ...extra,
+        });
+    };
+
+    require(`./handlers`)(bot);
+
+    bot.launch().then(() => {
+        console.log(`@${bot.context.botInfo.username} is running...`);
+    });
+})();
