6746bc4b8845050cc83bc42f5651bc85218a6d3b
Andrei Jiroh Eugenio Halili <andreijiroheugeniohalili24680@gmail.com>
2020-06-01 15:54:22 +0800

    Added ApiToken schema
    Signed-off-by: Andrei Jiroh Eugenio Halili <andreijiroheugeniohalili24680@gmail.com>

---
+++ models/api.js
@@ -0,0 +1,22 @@
+const mongoose = require(`mongoose`);
+
+const apiTokenSchema = new mongoose.Schema(
+    {
+        user_id: {
+            type: Number,
+            required: true,
+            unique: true,
+        },
+        apiKey: {
+            type: String,
+            required: true,
+        },
+        isRevoked: {
+            type: Boolean,
+        },
+    },
+    { timestamps: true },
+);
+
+const ApiToken = mongoose.model(`ApiToken`, apiTokenSchema);
+module.exports = ApiToken;
